8c0a7f4fd048c410e9591da9d33fd2c87b3f6c29
Mohamed Mahmoud <myworkmoud@gmail.com>
2026-04-22 18:50:52 +0200

    add Sparse Table

--- Rang_Query/Sparse_Table/Sparse_Table.cpp
+++ Rang_Query/Sparse_Table/Sparse_Table.cpp
@@ -1,1 +1,46 @@
-// SOON
+template <typename TP = ll>
+struct SparseTable {
+    int n;
+    vector <TP> v;
+    vector <vector <TP>> T;
+    function <TP (TP, TP)> merge;
+    TP SKIP;
+
+    SparseTable(vector <TP> _v, function <TP (TP, TP)> _merge, TP _skip) {
+        v = _v;
+        n = len(v);
+        SKIP = _skip;
+        merge = _merge;
+        T = vector <vector <TP>> (n + 5, vector <TP> (25, SKIP));
+        build();
+    }
+
+    void build() {
+        for (int i = 0; i < n; i++) {
+            T[i][0] = v[i];
+        }
+
+        for (int pw = 1; (1 << pw) <= n; pw++) {
+            for (int i = 0; i + (1 << pw) <= n; i++) {
+                T[i][pw] = merge(T[i][pw - 1], T[i + (1 << (pw - 1))][pw - 1]);
+            }
+        }
+    }
+
+    TP fastQuery(int l, int r) {
+        ll pw = __lg(r - l + 1);
+        return merge(T[l][pw], T[r - (1 << pw) + 1][pw]);
+    }
+
+    TP query (int l, int r) {
+        ll ret = SKIP;
+        ll sz = r - l + 1;
+        for (int pw = 23; pw >= 0; pw--) {
+            if (isON(sz, pw)) {
+                ret = merge(ret, T[l][pw]);
+                l += (1 << pw);
+            }
+        }
+        return ret;
+    }
+};
